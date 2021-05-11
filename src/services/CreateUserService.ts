b932f5d26a4f7c699d75a987b92d6f00d68e816e
Samuel Felipe <samuelfelipef@gmail.com>
2021-05-11 15:20:32 -0300

    alterando importação dos erros

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,7 +1,7 @@
 import { getRepository } from "typeorm";
 import User, { Gender } from "../models/User";
 import { hash } from 'bcryptjs';
-import AppError from "src/errors/AppError";
+import AppError from "../errors/AppError";
 
 
 interface Request {
